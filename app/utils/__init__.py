d6968588512c9292b09b9b3814d2fd283ab74132
vnzzzz <26959578+vnzzzz@users.noreply.github.com>
2026-08-30 18:35:50 +0900

    style: keep utility exports readable

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -5,9 +5,6 @@ utils パッケージ
 共通ユーティリティ関数とクラスを提供します。
 """
 
-# カレンダーデータ生成関連ユーティリティ
-# UI要素生成関連ユーティリティ
-# CSVデータ操作関連ユーティリティ
 from . import calendar_utils, csv_utils, ui_utils
 
 __all__ = ["calendar_utils", "ui_utils", "csv_utils"]
