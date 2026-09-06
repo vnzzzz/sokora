833f1c9a90ca9e1ab334a29b40a98686e6849ebf
vnzzzz <26959578+vnzzzz@users.noreply.github.com>
2026-09-06 16:52:26 +0900

    chore: user type full-list queryをformatter安定形にする

--- app/crud/user_type.py
+++ app/crud/user_type.py
@@ -38,9 +38,8 @@ class CRUDUserType(CRUDBase[UserType, UserTypeCreate, UserTypeUpdate]):
 
     def list_all(self, db: Session) -> List[UserType]:
         """paginationせず、全社員種別を``order``、次に名前順で取得します。"""
-        return db.query(UserType).order_by(
-            UserType.order.nullslast(), UserType.name
-        ).all()
+        query = db.query(UserType)
+        return query.order_by(UserType.order.nullslast(), UserType.name).all()
 
     def remove(self, db: Session, *, id: int) -> UserType:
         """未使用の社員種別を削除対象としてflushし、削除対象を返します。
